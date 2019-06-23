25d17078beaf44c2dd6c4dfdbaf670792750f64f
AJ Guiles <ashiguiles@gmail.com>
2019-06-23 20:40:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,19 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
    get '/name' do 
+     "My name is AJ"
     
   end
   
    get '/hometown' do 
+     
+     "My hometown is Denver"
+  
+  end
+  
+  get '/favorite_song' do 
+    
+    "My favorite song is ?"
   
   end
   
